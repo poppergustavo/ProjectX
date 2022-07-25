45a2be02ec59f84246e30338108b31ea0e9ca4fe
Gustavo Pöpper <gustavo.popper@philips.com>
2022-07-25 19:57:11 -0300

    250722update

--- Back-End/src/MainScripts.js
+++ Back-End/src/MainScripts.js
@@ -1,26 +1,8 @@
-window.setInterval(function(){
+document.getElementsByClassName("login_btn").onclick = function(){
 
-    var hora_atual = new Date();
+    console.log("test");
 
-    var hora = hora_atual.getHours();
-    var min = hora_atual.getMinutes();
-    var seg = hora_atual.getSeconds();
+    var username_v = document.getElementsByClassName("user_ipt").value;
+    document.getElementById("username_lbl").innerHTML = username_v;
 
-    function format_time(time){
-        if(time >= 0 && time <= 9){
-            var formatted_time = time.toString();
-            formatted_time = "0" + formatted_time;
-        }else{
-            return formatted_time;
-        }
-    }
-
-    document.getElementById("main_clock").innerHTML = hora.toString() + ":" + min.toString() + ":" + seg.toString();
-
-    if (seg < 10){
-        document.getElementById("main_clock").innerHTML = hora.toString() + ":" + min.toString() + ":0" + seg.toString(); 
-    }
-
-},1000);
-
-console.log("testing");
+};
