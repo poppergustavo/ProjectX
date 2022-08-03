2781fb7c9ea3675d43af17a09a0c19d322820a36
Gustavo Pöpper <gustavo.popper@philips.com>
2022-08-02 22:45:54 -0300

    Build 0.1-7 DEV

---
+++ Back-End/src/FunctionsScripts.js
@@ -0,0 +1,9 @@
+// SUMIR CONTEUDO
+
+document.getElementById("functions_btn").onclick = function(){
+
+    document.getElementById("main_img").style.display = "none";
+
+    document.getElementById("default_main_div").style.display = "flex";
+
+};
