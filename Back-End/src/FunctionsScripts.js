c5ef87a6e1fa976758e6e81b3bf5653d1d79190f
Gustavo Pöpper <gustavo.popper@philips.com>
2022-08-03 21:20:42 -0300

    0.1-8 DEV

--- Back-End/src/FunctionsScripts.js
+++ Back-End/src/FunctionsScripts.js
@@ -3,6 +3,50 @@
 document.getElementById("functions_btn").onclick = function(){
 
     document.getElementById("main_img").style.display = "none";
+    document.getElementById("adm_div").style.display = "none";
+    document.getElementById("rcrd_div").style.display = "none";
+    document.getElementById("db_div").style.display = "none";
+
+    document.getElementById("functions_div").style.display = "flex";
+    
+    document.getElementById("default_main_div").style.display = "flex";
+
+};
+
+document.getElementById("adm_btn").onclick = function(){
+
+    document.getElementById("main_img").style.display = "none";
+    document.getElementById("functions_div").style.display = "none";
+    document.getElementById("rcrd_div").style.display = "none";
+    document.getElementById("db_div").style.display = "none";
+
+    document.getElementById("adm_div").style.display = "flex";
+
+    document.getElementById("default_main_div").style.display = "flex";
+
+};
+
+document.getElementById("rcrd_btn").onclick = function(){
+
+    document.getElementById("main_img").style.display = "none";
+    document.getElementById("adm_div").style.display = "none";
+    document.getElementById("functions_div").style.display = "none";
+    document.getElementById("db_div").style.display = "none";
+
+    document.getElementById("rcrd_div").style.display = "flex";
+
+    document.getElementById("default_main_div").style.display = "flex";
+
+};
+
+document.getElementById("db_btn").onclick = function(){
+
+    document.getElementById("main_img").style.display = "none";
+    document.getElementById("adm_div").style.display = "none";
+    document.getElementById("functions_div").style.display = "none";
+    document.getElementById("rcrd_div").style.display = "none";
+
+    document.getElementById("db_div").style.display = "flex";
 
     document.getElementById("default_main_div").style.display = "flex";
 
